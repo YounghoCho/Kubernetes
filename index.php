d3ae71e4cc7344756332931ab607d76a3911e8c0
Youngho Jo <dodghek1@naver.com>
2018-01-15 17:09:21 +0900

    hands on

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Author" content="Youngho Jo">
+  <meta name="Description" content="Index page">
+  <title>DeveloperWorks Korea</title>
+ </head>
+ <body>
+	<img src="./Trick.jpg" style="width:100%;">  
+ </body>
+</html>
